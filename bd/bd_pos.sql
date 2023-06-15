9a28042d25e17fad6d5b4ce60f4cf7c4f44ef792
claudiowerner <claudiowernern@hotmail.com>
2023-06-15 11:14:14 -0400

    Cambio 01
    Eliminación de archivos respectivos a las mesas, pisos y ubicaciones

--- bd/bd_pos.sql
+++ bd/bd_pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-06-2023 a las 00:46:03
+-- Tiempo de generación: 15-06-2023 a las 02:49:54
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
