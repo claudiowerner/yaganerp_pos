5764786aa1f8b6b14b23893a3e2359a083784d2c
claudiowerner <claudiowernern@hotmail.com>
2023-07-18 14:12:12 -0400

    Cambio 55
    Se crea un nuevo login

--- app/index.php
+++ app/index.php
@@ -12,12 +12,14 @@
     <meta content="" name="author">
     <link href="ico/favicon.ico" rel="shortcut icon">
 
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" href="css/style.css">
 
 
     <title>YaganERP - Iniciar sesión</title>
-
-
-
     <style type="text/css">
 
         .bg
@@ -44,21 +46,7 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="css/theme.css">
-    <link rel="stylesheet" href="css/dashboard.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/dripicon.css">
-    <link rel="stylesheet" href="css/typicons.css" />
-    <link rel="stylesheet" href="css/font-awesome.css" />
-    <link rel="stylesheet" href="css/responsive.css">
-    <link rel="stylesheet" href="js/tip/tooltipster.css">
-    <link rel="stylesheet" type="text/css" href="js/vegas/jquery.vegas.css" />
-
-    <link rel="stylesheet" type="text/css" href="js/number-progress-bar/number-pb.css">
-    <!-- pace loader -->
-    <script src="js/pace/pace.js"></script>
-    <link href="js/pace/themes/orange/pace-theme-flash.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/bootstrap.css">
 
 
 
@@ -67,141 +55,73 @@
 
 
 <body>
-
-
-
     <div id="preloader">
-
-        <div id="status">&nbsp;</div>
-
-    </div>
-
-    <div class="error">
-
-        <span>Los datos ingresados son incorrectos. Intente nuevamente.</span>
-
-    </div>
-
-    <div class="container-fluid ">
-
-        <div class="row">
-
-            <div class="col-md-4 col-md-offset-4">
-
-                <!-- Comtainer -->
-
-                <div class="paper-wrap bevel tlbr">
-
-                    <div id="paper-top">
-
-                        <div class="row">
-
-                            <div class="col-lg-12 no-pad">
-
-                                <!--     -->
-
-                                <a class="navbar-brand logo-text" href="#">YaganERP</a>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-
-
-                    <!-- CONTENT -->
-
-                    <div style="min-height:400px;" class="wrap-fluid" id="paper-bg">
-
-
-
-                        <div class="row">
-
-                            <div class="col-lg-12">
-
-                                <div class="account-box">
-
-                                    <form id="Frm" action="">
-
-                                        <div class="form-group">
-
-                                            <label>Nombre</label>
-
-                                            <input name="t_user" required placeholder="Usuario" type="text" class="form-control" autofocus autocomplete="off">
-
-                    
-
-                                        </div>
-
-                                        <div class="form-group">
-
-                                            <a href="#" class="pull-right label-forgot">¿Olvidó su contraseña?</a>
-
-                                            <label>Contraseña</label>
-
-                                            <input name="t_pass" required placeholder="Contraseña" type="password" class="form-control">
-
-                                        </div>
-
-                                        <div class="checkbox pull-left">
-
-                                            <label>
-
-                                                <input type="checkbox">Remember me
-
-                                            </label>
-
-                                        </div>
-
-                                        <button class="btn btn-primary btn-block botonlg">Ingresar al sistema</button>
-
-                                    </form>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <!-- / FOOTER -->
-
-                <!-- Container -->
-
-            </div>
-
+        <div id="status">&nbsp;
+            Cargando...
         </div>
-
     </div>
+
+    <section class="ftco-section ">
+		<div class="container" style="">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5 col-lg-12">
+					<h2 class="heading-section"><strong>Bienvenido a YaganERP POS&nbsp;</strong> </h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+                <div class="col-md-6 col-lg-5" class="responsive">
+                    <img src="img/shopping.jpg" width="701" srcset="">
+                </div>
+				<div class="col-md-5 col-lg-5 col-lg-offset-2">
+                    
+					<div class="login-wrap p-4 p-md-5">
+                        <div class="icon d-flex align-items-center justify-content-center">
+                            <span class="fa fa-user-o"></span>
+                        </div>
+                        <form id="Frm" action="">
+                            <div class="form-group">
+                                <label>Nombre</label>
+                                <input name="t_user" required placeholder="Usuario" type="text" class="form-control" autofocus autocomplete="off">
+                            </div>
+                            <div class="form-group">
+                                <a href="#" class="pull-right label-forgot">¿Olvidó su contraseña?</a>
+                                <label>Contraseña</label>
+                                <input name="t_pass" required placeholder="Contraseña" type="password" class="form-control">
+                            </div>
+                            <div class="checkbox pull-left">
+                                <label>
+                                    <input type="checkbox">Remember me
+                                </label>
+                            </div>
+                            <button class="btn btn-primary btn-block botonlg">Iniciar sesión&nbsp;</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</section>
 
     <!-- 
 
     ================================================== -->
 
     <!-- Main jQuery Plugins -->
-
     <script type='text/javascript' src="js/jquery.js"></script>
-
     <script type='text/javascript' src='js/vegas/jquery.vegas.js'></script>
-
     <script type='text/javascript' src='js/image-background.js'></script>
-
     <script type="text/javascript" src="js/jquery.tabSlideOut.v1.3.js"></script>
-
     <script type="text/javascript" src="js/bg-changer.js"></script>
 
     
-
     <script type="text/javascript" src="css/main.js"></script>
-
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-
+    
+    <script src="js/bootstrap.bundle.min.js"></script>
     <script src="main.js"></script>
-
+    <script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
 
 
 
@@ -213,3 +133,18 @@
 
 
 </html>
+
+
+
+
+
+
+
+
+
+
+                                    <div class="error">
+
+        <span>Los datos ingresados son incorrectos. Intente nuevamente.</span>
+
+    </div>
