74ee73dcf146214f931cf9d2046795e8273dd694
claudiowerner <40247488+claudiowerner@users.noreply.github.com>
2024-07-16 16:17:31 -0400

    Cambio 197
    Actualización de tablas

--- app/sys/users/admin/reporteProducto/js/tabla/tablas_sin_filtrar/categorias_sin_filtrar.js
+++ app/sys/users/admin/reporteProducto/js/tabla/tablas_sin_filtrar/categorias_sin_filtrar.js
@@ -8,6 +8,6 @@ function cargarTablaCategoriasSinFiltrar()
         template += `<tr><td>${j.nombre_categoria}</td><td>${j.cantidad}</td></tr>`;
         total_ventas = parseInt(total_ventas) + parseInt(j.cantidad)
     })
-    template += `<tr><td><strong>Total de ventas</strong></td><td>${total_ventas}</td></tr>`;
+    template += `<tr><td><strong>Total de ventas</strong></td><td><strong>${total_ventas}</strong></td></tr>`;
     $("#tbodyCategorias").html(template)
 }
--- app/sys/users/admin/reporteProducto/js/tabla/tablas_sin_filtrar/productos_sin_filtrar.js
+++ app/sys/users/admin/reporteProducto/js/tabla/tablas_sin_filtrar/productos_sin_filtrar.js
@@ -5,7 +5,7 @@ function cargarTablaProductosSinFiltrar()
     let descarga = descargarInfoGraficoBarraProductosSinFiltrar()
     let json = JSON.parse(descarga);
     let template = "";
-    console.log(json);
+    let valor_total = 0;
     json.forEach(j=>{
         let dinero_generado = formatearNumero("P",j.valor);
         template+=
@@ -13,8 +13,14 @@ function cargarTablaProductosSinFiltrar()
             <td><strong>${j.nombre_producto}</strong></td>
             <td>${j.cantidad}</td>
             <td>${dinero_generado}</td>
-        </tr>`
+        </tr>`;
+        valor_total = parseInt(valor_total) + parseInt(j.valor);
     })
-
+    let valor_total_formateado = formatearNumero("P", valor_total);
+    template += 
+    `<tr>
+        <td colspan=2><strong>Valor total:</strong></td>
+        <td><strong>${valor_total_formateado}</strong></td>
+    </tr>`;
     $("#tbodyProductos").html(template);
 }
