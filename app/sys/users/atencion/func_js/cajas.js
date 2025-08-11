58f205dc9c7d9ce9a698e99f57650af0eade8cf8
claudiowerner <40247488+claudiowerner@users.noreply.github.com>
2025-08-11 16:46:30 -0400

    Corrección apartado de ventas
    Se agregó un mensaje de que no existen cajas al momento de entrar a ventas y verificar no hay cajas creadas o eliminadas

--- app/sys/users/atencion/func_js/cajas.js
+++ app/sys/users/atencion/func_js/cajas.js
@@ -1,40 +1,48 @@
 
 function obtener_cajas()
 {
-  $.ajax(
-  {
-    url: 'func_php/read_cajas.php',
-    type: 'GET',
-    success: function(response)
-    {
-      let tasks = JSON.parse(response);
-      let template = '';
+	$.ajax(
+	{
+		url: 'func_php/read_cajas.php',
+		type: 'GET',
+		success: function(response)
+		{
+			let tasks = JSON.parse(response);
+			let template = '';
 
-      //Se valida si la variable tasks es un arreglo válido o no
-      if(Array.isArray(tasks))
-      {
-        tasks.forEach(task=>{
-          clase = "";
-          if(task.estado=="S")
-          {
-            clase = "btn-primary";
-          }
-          if(task.estado=="A")
-          {
-            clase = "btn-danger";
-          }
-          template+=
-          `<button class='btn ${clase}'  style='margin: 2px'  nroCaja=${task.id} nomCaja=${task.nombre}><h1>${task.nombre}</h1></button>`;
-        });
-      }
-      $("#cajas").html(template);
-    }
-  })
-  .done( function() {
-    console.log( 'Success!!' );
-  }).fail( function(resp) {
-    console.log( 'Error!! '+resp.ResponseText );
-  }).always( function() {
-    console.log( 'Always' );
-  });
+			//Se valida si la variable tasks es un arreglo válido o no
+			if(Array.isArray(tasks))
+			{
+				tasks.forEach(task=>{
+				clase = "";
+				if(task.estado=="S")
+				{
+					clase = "btn-primary";
+				}
+				if(task.estado=="A")
+				{
+					clase = "btn-danger";
+				}
+				template+=
+					`<button class='btn ${clase}'  style='margin: 2px'  nroCaja=${task.id} nomCaja=${task.nombre}><h1>${task.nombre}</h1></button>`;
+				});
+				$("#cajas").html(template);
+			}
+			else
+			{
+				if(!tasks.cajas_existentes)
+				{
+					$("#alert_cajas").show();
+					$("#cajas").hide();
+				}
+			}
+		}
+	})
+	.done( function() {
+		console.log( 'Success!!' );
+	}).fail( function(resp) {
+		console.log( 'Error!! '+resp.ResponseText );
+	}).always( function() {
+		console.log( 'Always' );
+	});
 };
--- app/sys/users/atencion/func_php/read_cajas.php
+++ app/sys/users/atencion/func_php/read_cajas.php
@@ -1,38 +1,46 @@
 <?php
 
-session_start();
+	session_start();
 
-if(isset($_SESSION['user'])){
-  $tipo = $_SESSION['user']['tipo_usuario'];
-
-
-  $id_us = $_SESSION['user']['id'];
-  $nombre = $_SESSION['user']["nombre"];
-  $id_cl = $_SESSION['user']["id_cl"];
-  
+	if(isset($_SESSION['user']))
+	{
+		$tipo = $_SESSION['user']['tipo_usuario'];
 
 
-  require_once '../../../conexion.php';
+		$id_us = $_SESSION['user']['id'];
+		$nombre = $_SESSION['user']["nombre"];
+		$id_cl = $_SESSION['user']["id_cl"];
+		
 
 
+		require_once '../../../conexion.php';
+		
+		
+		$sql = 
+		"SELECT * FROM cajas 
+		WHERE id_cl = $id_cl
+		AND estado!='N'";
+		$resultado = $conexion->query($sql);
 
-    //query
-    $sql = 
-    "SELECT * FROM cajas 
-    WHERE id_cl = $id_cl
-    AND estado!='N'";
-  $resultado = $conexion->query($sql);;
-  if ($resultado->num_rows > 0){
-    $json = array();
-    while ($row = $resultado->fetch_array()) {
-      $json[] =array(
-          'id' => $row['id'],
-          'nombre' => $row['nom_caja'],
-          'estado' => $row['estado']
-        );
-    }
-    echo json_encode($json);
-  }
-}
+		if ($resultado->num_rows > 0)
+		{
+			while ($row = $resultado->fetch_array())
+			{
+				$json[] =array(
+					"cajas_existentes" => true,
+					'id' => $row['id'],
+					'nombre' => $row['nom_caja'],
+					'estado' => $row['estado']
+				);
+			}
+		}
+		else
+		{
+			$json = array(
+				"cajas_existentes" => false
+			);
+		}
+		echo json_encode($json);
+	}
 
 ?>
